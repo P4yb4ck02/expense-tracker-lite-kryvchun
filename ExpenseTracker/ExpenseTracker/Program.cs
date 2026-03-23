54f3ae17d7078d6a55f277fa0c7459204ab91a62
P4yb4ck02 <vlad.krivchun93@gmail.com>
2026-03-23 02:31:55 +0200

    init project structure

---
+++ ExpenseTracker/ExpenseTracker/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ExpenseTrackerLite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var expenses = new List<decimal>();
+            Console.WriteLine("Expense Tracker Lite");
+        }
+    }
+}
