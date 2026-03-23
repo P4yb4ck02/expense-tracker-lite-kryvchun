020ee5895731ad75f27fe68af4af091cf0744895
P4yb4ck02 <vlad.krivchun93@gmail.com>
2026-03-23 02:37:48 +0200

    add core logic

---
+++ ExpenseTracker/ExpenseTracker/ExpenseService.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+
+namespace ExpenseTrackerLite
+{
+    public class ExpenseService
+    {
+        private readonly List<decimal> _expenses = new List<decimal>();
+
+        public void AddExpense(decimal amount)
+        {
+            _expenses.Add(amount);
+        }
+
+        public decimal GetTotal()
+        {
+            decimal total = 0;
+            foreach (var expense in _expenses)
+            {
+                total += expense;
+            }
+            return total;
+        }
+    }
+}
