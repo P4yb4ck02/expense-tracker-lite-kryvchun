88b60e391ed5da03849df8339846eb1263286261
P4yb4ck02 <vlad.krivchun93@gmail.com>
2026-04-04 18:40:43 +0300

    fix build entry point and break test for feedback loop

--- ExpenseTracker/ExpenseTracker/ExpenseTests.cs
+++ ExpenseTracker/ExpenseTracker/ExpenseTests.cs
@@ -20,7 +20,7 @@ namespace ExpenseTrackerLite.Tests
             decimal actualTotal = service.GetTotal();
 
             // Assert 
-            Assert.Equal(expectedTotal, actualTotal);
+            Assert.Equal(expectedTotal + 100, actualTotal);
         }
 
         [Theory]
--- ExpenseTracker/ExpenseTracker/obj/Debug/net10.0/ExpenseTracker.AssemblyInfo.cs
+++ ExpenseTracker/ExpenseTracker/obj/Debug/net10.0/ExpenseTracker.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28db234e2e6f55a22e2b0d9081e05958aaba5d68")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1082a757bff2e8f0caaa8327855529e4812724f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ExpenseTracker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
